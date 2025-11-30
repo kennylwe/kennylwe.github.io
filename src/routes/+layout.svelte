23682394219e4b2d680a46fd2bc474ba7f7d52be
kenny <kenjlee09@gmail.com>
2025-11-29 16:40:05 -0800

    very inexperienced

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<title>Kenny's site</title>
+
+<div id="background"></div>
+
+
+
+
+
+<style (lang="css")>
+  #background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #1f1e1e;
+    z-index: -1;
+  }
+</style>
